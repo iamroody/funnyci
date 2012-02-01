29734eac18c526e140337057b8feb5dc7eea530d
roody <wangyan87@gmail.com>
2012-02-01 22:56:17 +0800

    add check env

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+function env {
+    pip install -r requirements.pip
+}
+
 function run_test {
     nosetests;
 }
@@ -14,12 +19,14 @@ function show_help {
   echo "Usage: build.sh [COMMAND]"
   echo ""
   echo "COMMAND:"
+  echo -e "env: check env and install dependency"
   echo -e "test: run all test"
   echo -e "run: get status of ci, and send message to weibo or twitter"
 }
 
 function main {
 	case $1 in
+	    env) env;;
 		test) run_test;;
 		run) run;;
 		*) show_help ; exit 1;;
