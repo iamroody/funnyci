b8505f97fa961ebc661e76168f53b96c80eca00e
roody <wangyan87@gmail.com>
2012-02-02 15:03:44 +0800

    add version logic for detecting the build version changes

--- pygoboard.py
+++ pygoboard.py
@@ -5,12 +5,14 @@ from xml.dom import minidom
 
 jobs = ['package', 'acceptance_standalone', 'acceptance_staging']
 go_url = 'http://go.hi-ci.vpc.realestate.com.au:8153/go/cctray.xml'
+oldBuildVersions = {}
 
 def get_status_from_xml():
-    document = urllib2.urlopen(urllib2.Request(go_url)).read()
-
+#    document = urllib2.urlopen(urllib2.Request(go_url)).read()
+    document = open("building-go.xml").read()
     dom = minidom.parseString(document)
     stageStatus = {}
+    buildVersions = {}
 
     for node in dom.getElementsByTagName('Project'):
         stage = node.getAttribute('name')
@@ -18,9 +20,12 @@ def get_status_from_xml():
             if stage == 'home-ideas :: ' + job:
                 activity = node.getAttribute('activity')
                 last_build_status = node.getAttribute('lastBuildStatus')
+                buildVersions[job] = node.getAttribute('lastBuildLabel')
                 stageStatus[job] = get_stage_status(activity, last_build_status)
-    return stageStatus
+    return stageStatus,buildVersions
 
+def is_build_version_changed(newBuildVersions, oldBuildVersions):
+    return not newBuildVersions.__eq__(oldBuildVersions)
 
 def get_stage_status(activity, last_build):
     if activity == 'Building':
@@ -31,25 +36,27 @@ if __name__ == '__main__':
     try:
         socket.setdefaulttimeout(5)
 
-        go_status = get_status_from_xml()
+        go_status,currentBuildVersions = get_status_from_xml()
 
-        build_status = "off"
+        if is_build_version_changed(currentBuildVersions, oldBuildVersions):
+            print "first "
+            oldBuildVersions = currentBuildVersions
 
-        print "*** running ***"
+            build_status = "off"
 
-        if all(go_status[job] == 'success' for job in jobs):
-            build_status = 'success'
-        elif any(go_status[job] == 'building' for job in jobs):
-            if any(go_status[job] == 'failure' for job in jobs):
-                build_status = 'warning'
+            print "*** running ***"
+
+            if all(go_status[job] == 'success' for job in jobs):
+                build_status = 'success'
+            elif any(go_status[job] == 'building' for job in jobs):
+                if any(go_status[job] == 'failure' for job in jobs):
+                    build_status = 'warning'
+                else:
+                    build_status = 'building'
+            elif any(go_status[job] == 'failure' for job in jobs):
+                build_status = 'failure'
             else:
-                build_status = 'building'
-        elif any(go_status[job] == 'failure' for job in jobs):
-            build_status = 'failure'
-        else:
-            build_status = 'warning'
-
-        print "currnet status:" + build_status
-
+                build_status = 'warning'
+        print "not changed"
     except Exception, (error):
         traceback.print_exc(file=sys.stdout)
--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,1 @@
 import unittest
-
-class TestIndex(unittest.TestCase):
-    def test_shuffle(self):
-        self.assertEqual(1, 2)
