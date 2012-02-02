7681956a6b3f54677defe636f5c71fab517f5f08
roody <wangyan87@gmail.com>
2012-02-02 17:17:03 +0800

    refactor pygoboard

--- pygoboard.py
+++ pygoboard.py
@@ -5,6 +5,42 @@ import traceback
 from parser import Parser
 from static import jobs
 
+
+def getOldVersion():
+    f = open('tmp/buildversion', 'r')
+    version = pickle.load(f)
+    f.close()
+    return version
+
+
+def writeVersionToFile(currentBuildVersions):
+    f = open('tmp/buildversion', 'wb')
+    pickle.dump(currentBuildVersions, f)
+    f.close()
+
+
+def is_build_version_changed(currentBuildVersions):
+    oldVersion = getOldVersion()
+    if not oldVersion.__eq__(currentBuildVersions):
+        writeVersionToFile(currentBuildVersions)
+        return True
+    return False
+
+
+def getBuildStatus(go_status):
+    if all(go_status[job] == 'success' for job in jobs):
+        build_status = 'success'
+    elif any(go_status[job] == 'building' for job in jobs):
+        if any(go_status[job] == 'failure' for job in jobs):
+            build_status = 'warning'
+        else:
+            build_status = 'building'
+    elif any(go_status[job] == 'failure' for job in jobs):
+        build_status = 'failure'
+    else:
+        build_status = 'warning'
+    return build_status
+
 if __name__ == '__main__':
     try:
         socket.setdefaulttimeout(5)
@@ -12,36 +48,14 @@ if __name__ == '__main__':
         document = open("building-go.xml").read()
 
         parser = Parser()
+        ciModel = parser.get_ci_model_from_xml_string(document)
+        go_status = ciModel.get_stage_status()
+        currentBuildVersions = ciModel.get_build_version()
 
-        go_status = parser.get_ci_model_from_xml_string(document).get_stage_status()
-        currentBuildVersions = parser.get_ci_model_from_xml_string(document).get_build_version()
-
-        f = open('tmp/buildversion', 'wb')
-        pickle.dump(currentBuildVersions, f)
-        f.close()
-
-        f = open('tmp/buildversion', 'r')
-        test = pickle.load(f)
-        f.close()
-
-        oldBuildVersions = currentBuildVersions
-
-        build_status = "off"
-
-        print "*** running ***"
-
-        if all(go_status[job] == 'success' for job in jobs):
-            build_status = 'success'
-        elif any(go_status[job] == 'building' for job in jobs):
-            if any(go_status[job] == 'failure' for job in jobs):
-                build_status = 'warning'
-            else:
-                build_status = 'building'
-        elif any(go_status[job] == 'failure' for job in jobs):
-            build_status = 'failure'
-        else:
-            build_status = 'warning'
-
-        print build_status
+        if is_build_version_changed(currentBuildVersions):
+            build_status = "off"
+            print "*** running ***"
+            build_status = getBuildStatus(go_status)
     except Exception, (error):
         traceback.print_exc(file=sys.stdout)
+
