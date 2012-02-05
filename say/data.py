8ecd22c96b7934a3c61a7c8b8ba6e7e7dd0fec38
roody <wangyan87@gmail.com>
2012-02-05 23:07:08 +0800

    modify words

--- say/data.py
+++ say/data.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 Say_Message = {
-    "building": u"A new commit is running! Good luck! ",
-    "warning": u"So sad, last commit is failed, Some One is fixing now! Cross your finger! Let's Bless",
+    "building": u"A new commit is running, Good luck.",
+    "warning": u"So sad! last commit is failed! Some One is fixing now! Cross your finger! Bless!",
     "success": u"oh yeah! Current commit is success! ",
-    "failure": u"Warning! Warning! Warning! Build Broken! Hope some one could help me!"
+    "failure": u"Warning,Warning,Warning,Build Broken,Hope someone could help me!"
 }
